5b046dd3e9ff76bc0a2cb4e0e7e5bea5c777c62b
Beans-and-Rice <dev.beansandrice@gmail.com>
2018-02-20 09:12:47 +1300

    message

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title>Work Mate</title>
-
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/grid.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
 	<script type="text/javascript" src="assets/js/script.js"></script>
@@ -17,14 +18,27 @@
 </head>
 <body>
 
-
-	<header>
+	<aside>
 		<nav>
+			<img src="assets/img/logo.php">
 			<ul>
-				<li><a href=""></a></li>
+				<li><a href="">Dashboard</a></li>
+				<li><a href="">Projects</a></li>
+				<li><a href="">Reports</a></li>
+				<li><a href="">Clients</a></li>
+				<li><a href="">Team</a></li>
 			</ul>
 		</nav>
-	</header>
+	</aside>
+
+	<main class="right_side">
+
+		<header>
+			
+		</header>
+		
+	</main>
+
 
 
 
